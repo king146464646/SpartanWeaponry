09fd9e48e6f3ec6663dfd91e3db3e258f90b4989
king146464646 <hs52wo@gmail.com>
2026-09-09 18:38:42 +0800

    Fix abnormal player behavior when holding non-vanilla crosshair weapons with Shoulder Surfing compatibility

--- src/main/java/org/xiyu/spartanweaponryunofficial/compat/shouldersurfing/ShoulderSurfingPlugin.java
+++ src/main/java/org/xiyu/spartanweaponryunofficial/compat/shouldersurfing/ShoulderSurfingPlugin.java
@@ -2,11 +2,8 @@ package org.xiyu.spartanweaponryunofficial.compat.shouldersurfing;
 
 import com.github.exopandora.shouldersurfing.api.plugin.IShoulderSurfingPlugin;
 import com.github.exopandora.shouldersurfing.api.plugin.IShoulderSurfingRegistrar;
-import org.xiyu.spartanweaponryunofficial.api.tags.ModItemTags;
 
 public class ShoulderSurfingPlugin implements IShoulderSurfingPlugin {
     @Override
-    public void register(IShoulderSurfingRegistrar registrar) {
-        registrar.registerAdaptiveItemCallback(stack -> stack.is(ModItemTags.HAS_CUSTOM_CROSSHAIR));
-    }
+    public void register(IShoulderSurfingRegistrar registrar) {}
 }
